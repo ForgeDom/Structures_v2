3d3d9909ab41fe5586890f122e4f284d2e4d1cc9
Max <maxstill2006@gmail.com>
2024-07-06 17:45:49 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-#include <Windows.h>
+#include <cstring>
+
 using namespace std;
 
-int main() {
+struct Animal {
+    char name[50];
+    char classification[50];
+    char nickname[50];
+};
 
-	return 0;
+void fillAnimal(Animal& animal, const char* name, const char* classification, const char* nickname) {
+    strcpy_s(animal.name, name);
+    strcpy_s(animal.classification, classification);
+    strcpy_s(animal.nickname, nickname);
+}
+
+void displayAnimal(const Animal& animal) {
+    cout << "Animal Name: " << animal.name << endl;
+    cout << "Classification: " << animal.classification << endl;
+    cout << "Nickname: " << animal.nickname << endl;
+}
+
+void makeSound(const Animal& animal) {
+    cout << animal.nickname << " is making a sound!" << endl;
+}
+
+int main() {
+    Animal myAnimal;
+    fillAnimal(myAnimal, "Reks", "Dog", "Buddy");
+
+    displayAnimal(myAnimal);
+    makeSound(myAnimal);
+
+    return 0;
 }
